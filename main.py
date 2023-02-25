c5eb71cff54e54527c17925f8b62b7091c438db3
Baraded <borodeda@gmail.com>
2023-02-26 01:07:11 +0300

    Ex_4 исправил под условие задачи

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 storona_1 = int(input("Введите количество ячеек на первой стороне шоколадки: "))
 storona_2 = int(input("Введите количество ячеек на второй стороне шоколадки: "))
-storona_otlom_kuska =int(input("Какой кусок шоколада ты хочешь отломать? "))
-if storona_otlom_kuska == storona_2 or storona_otlom_kuska == storona_1:
+i_want_choco =int(input("Какой кусок шоколада ты хочешь отломать? "))
+if i_want_choco % storona_1 == 0 or i_want_choco % storona_2 == 0:
     print("Ставь кофе, шоколадка твоя")
 else:
     print("Что то пошло не так и ты останешься без шоколада")
