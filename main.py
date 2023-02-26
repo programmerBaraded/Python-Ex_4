32ec3ce31ef07bc2d8e22db405f8d2a6eb4f9b8b
Baraded <borodeda@gmail.com>
2023-02-27 00:11:38 +0300

    Ex_4 добавил проверку на всю шоколадку

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 storona_1 = int(input("Введите количество ячеек на первой стороне шоколадки: "))
 storona_2 = int(input("Введите количество ячеек на второй стороне шоколадки: "))
 i_want_choco =int(input("Сколько кусочков шоколада ты хочешь отломать? "))
-if i_want_choco % storona_1 == 0 or i_want_choco % storona_2 == 0:
+if storona_2 * storona_1 > i_want_choco and (i_want_choco % storona_1 == 0 or i_want_choco % storona_2 == 0):
     print("Ставь кофе, шоколадка твоя")
 else:
     print("Что то пошло не так и ты останешься без шоколада")
